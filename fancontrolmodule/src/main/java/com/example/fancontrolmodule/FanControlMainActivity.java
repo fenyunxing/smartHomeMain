33bc4159c955b27dcb7ac8b61caa58bcfe9969dc
fenyunxing <13977316620@163.com>
2020-07-06 22:13:30 +0800

    9 增加风扇界面的定时显示 10 增加定时控制按钮

---
+++ fancontrolmodule/src/main/java/com/example/fancontrolmodule/FanControlMainActivity.java
@@ -0,0 +1,14 @@
+package com.example.fancontrolmodule;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FanControlMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fan_control_main);
+    }
+}
